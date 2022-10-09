2a0f8a6f29c96d2b9bac33ae14b808cf710afdf3
Vynicius Pontes <vpontes@addi.com>
2022-10-09 10:44:44 -0300

    Fix InvalidVOException construction

--- src/main/java/br/com/elo7/sonda/candidato/domain/exception/InvalidVOException.java
+++ src/main/java/br/com/elo7/sonda/candidato/domain/exception/InvalidVOException.java
@@ -2,7 +2,9 @@ package br.com.elo7.sonda.candidato.domain.exception;
 
 public class InvalidVOException extends DomainException{
 
+  private static final String MESSAGE = "Invalid %s";
+
   public InvalidVOException(String className) {
-    super(className, "Invalid %s");
+    super(className, String.format(MESSAGE, className));
   }
 }
